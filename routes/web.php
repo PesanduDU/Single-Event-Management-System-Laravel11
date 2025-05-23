d8691ed1fa7295d1d19da5a03075c9799f3bead1
PesanduDU <pesandudubeysekara@gmail.com>
2025-05-23 12:59:03 +0530

    Admin login panel
    Added the admin panel login

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,13 @@
 <?php
 
+use App\Http\Controllers\Admin\AdminAuthController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+//Admin
+Route::prefix('admin')->group(function () {
+    Route::get('/login', [AdminAuthController::class, 'login'])->named('admin_login');
 });
